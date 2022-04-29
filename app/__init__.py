849441d6fb4a2b7dcb1d21df51e7c8c843eb1da5
aust1inn <austinotieno364@gmail.com>
2022-04-29 14:02:49 +0300

    tests start.sh to ensure basic setup is done properly

--- app/__init__.py
+++ app/__init__.py
@@ -20,8 +20,8 @@ def create_app(config_name):
     app.register_blueprint(main_blueprint)
 
     # setting config
-    from .requests import configure_request
-    configure_request(app)
+    # from .requests import configure_request
+    # configure_request(app)
 
 
     # Will add the views and forms
