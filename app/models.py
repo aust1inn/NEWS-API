0f62026207d09febafde41495c76fa95b9be7ccd
aust1inn <austinotieno364@gmail.com>
2022-04-30 10:33:16 +0300

    creates a blueprint for headlines

--- app/models.py
+++ app/models.py
@@ -8,11 +8,18 @@ class Source:
         self.description = description
         self.url = url
 
+class Headlines:
+   
+    def __init__(self,author,description,time,url,image,title):
+        self.author = author
+        self.description = description
+        self.time = time
+        self.url = url
+        self.image = image
+        self.title = title
         
 class Article:
-    '''
-    Class that instantiates objects of the news article objects of the news sources
-    '''
+   
     def __init__(self,author,description,time,url,image,title):
         self.author = author
         self.description = description
