cd3b9b2211887ee9a162335783cea4d6d73e17b0
aust1inn <austinotieno364@gmail.com>
2022-04-30 16:51:39 +0300

    sets up news.html that uses dynamic routing

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,7 @@
 from flask import render_template,request,redirect,url_for
 
 from . import main
-from ..requests import get_source,get_headlines
+from ..requests import get_source,get_headlines,article_source
 
 @main.route('/')
 def index():
@@ -9,3 +9,12 @@ def index():
     source =get_source()
     headline=get_headlines()
     return render_template('index.html',sources=source,headlines=headline)
+
+@main.route('/article/<id>')
+def article(id):
+
+    '''
+    View article page function that returns the various article details page and its data
+    '''
+    articles = article_source(id)
+    return render_template('news.html',articles= articles,id=id )
